2ac1a13633fcb51d8c7757d9ebadeae787338c28
Dylan Vu <dylan.n.vu@gmail.com>
2016-12-05 14:45:18 -0800

    add ajax call to grab concerts from bandsintown API

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,53 @@
 (function() {
   'use strict';
 
-  $(".button-collapse").sideNav();
-  $('select').material_select();
+  $('.button-collapse').sideNav();
 
+  // $('select').material_select();
 
+  const getConcerts = function(city) {
+    const $xhr = $.ajax({
+      method: 'GET',
+      url: `https://cors-anywhere.herokuapp.com/http://api.bandsintown.com/events/search.json?api_version=2.0&app_id=Local&location=${city}&radius=10`,
+      dataType: 'json'
+    });
+
+    $xhr.done((data) => {
+      if ($xhr.status !== 200) {
+        return;
+      }
+
+      const currentDate = new Date();
+
+      for (const result of data) {
+        const concertDate = new Date(result.datetime);
+
+        if (currentDate.getDate() !== concertDate.getDate()) {
+          return;
+        }
+
+        const concert = {
+          url: result.url,
+          date: concertDate,
+          artist: result.artists[0],
+          venue: {
+            name: result.venue.name,
+            city: result.venue.city,
+            state: result.venue.region,
+            country: result.venue.country
+          }
+        };
+
+        getArtistInfo(concert);
+      }
+    });
+
+    $xhr.fail((err) => {
+      console.log(err);
+    });
+  };
+
+  $('form').on('submit', () => {
+    getConcerts($('#location').val());
+  });
 })();
